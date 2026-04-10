65f01ca1b1b7c6702d04ea6b6bc1218ec7508189
edumendes1337 <76907135+edumendes1337@users.noreply.github.com>
2026-04-10 07:12:05 -0300

    Delete front-main directory

--- front-main/src/contexts/authContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-interface AuthContextProps {
-  isLoggedIn: boolean;
-  login: () => void;
-  logout: () => void;
-}
-
-export const AuthContext = createContext<AuthContextProps | undefined>(undefined);
